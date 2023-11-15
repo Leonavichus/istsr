fc908cc01413889ac5a5adb21325df2d6b6bb67d
Yoshi <dleonavichus@mail.ru>
2023-11-15 23:32:26 +0300

    Minor fix

--- normal.py
+++ normal.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
 
 mu = 12
 sigma = 2.7
@@ -9,3 +12,25 @@ sample = np.random.normal(mu, sigma, sample_size)
 
 # Вывод выборки
 print(sample)
+
+# Описательная статистика
+print("Описательная статистика:")
+print("Среднее:", np.mean(sample))
+print("Стандартная ошибка:", np.std(sample) / np.sqrt(len(sample)))
+print("Медиана:", np.median(sample))
+print("Мода:", stats.mode(sample))
+print("Стандартное отклонение:", np.std(sample))
+print("Дисперсия выборки:", np.var(sample))
+print("Эксцесс:", stats.kurtosis(sample))
+print("Асимметричность:", stats.skew(sample))
+print("Интервал:", )
+print("Минимум:", np.amin(sample))
+print("Максимум:", np.amax(sample))
+print("Сумма:", np.sum(sample))
+print("Счет:", len(sample))
+print("Уровень надежности:", )
+
+# Гистограмма
+sns.histplot(sample, kde=True)
+plt.title('Гистограмма')
+plt.show()
