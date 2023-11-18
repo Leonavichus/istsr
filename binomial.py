b2e787d2d3635a7b5d9056ae767bd144cd417858
Yoshi <dleonavichus@mail.ru>
2023-11-18 19:17:14 +0300

    stats

--- binomial.py
+++ binomial.py
@@ -1,11 +1,69 @@
 import numpy as np
+import seaborn as sns
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 m = 5
 p = 0.9
 sample_size = 50
 
 # Генерация выборки
-sample = np.random.binomial(m, p, sample_size)
+sample = np.random.binomial(n=m, p=p, size=sample_size)
 
-# Вывод выборки
-print(sample)
+# Описательная статистика
+print('Описательная статистика:')
+print('Среднее:', np.mean(sample))
+print('Стандартная ошибка:', np.std(sample) / np.sqrt(len(sample)))
+print('Медиана:', np.median(sample))
+print('Мода:', stats.mode(sample))
+print('Стандартное отклонение:', np.std(sample))
+print('Дисперсия выборки:', np.var(sample))
+print('Эксцесс:', stats.kurtosis(sample))
+print('Асимметричность:', stats.skew(sample))
+print('Доверительный интервал для математического ожидания (95,0%):', stats.t.interval(confidence=0.95, df=len(
+    sample)-1, loc=np.mean(sample), scale=stats.sem(sample)))
+print('Минимум:', np.amin(sample))
+print('Максимум:', np.amax(sample))
+print('Сумма:', np.sum(sample))
+print('Счет:', len(sample))
+print('Доверительный интервал для дисперсии (95,0%):',  stats.t.interval(confidence=0.95, df=len(sample)-1,
+      loc=np.var(sample), scale=2*np.var(sample)/len(sample)))
+
+plt.figure(figsize=(14, 10))
+
+# Гистограмма
+plt.subplot(2, 3, 1)
+sns.histplot(sample, kde=True)
+plt.title('Гистограмма')
+plt.xlabel('Значение')
+plt.ylabel('Частота')
+
+# Полигон
+plt.subplot(2, 3, 2)
+sns.kdeplot(sample, cumulative=False)
+plt.title('Полигон')
+plt.xlabel('Значение')
+plt.ylabel('Плотность')
+
+# Кумулянта
+plt.subplot(2, 3, 3)
+sns.kdeplot(sample, cumulative=True)
+plt.title('Кумулянта')
+plt.xlabel('Значение')
+plt.ylabel('Вероятность')
+
+# Ящик с усами
+plt.subplot(2, 3, 4)
+sns.boxplot(sample)
+plt.title('Ящик с усами')
+plt.xlabel('Значение')
+
+# Q-Q Plot
+plt.subplot(2, 3, 5)
+stats.probplot(sample, plot=plt)
+plt.title('Q-Q Plot')
+plt.xlabel('Теоретические квантили')
+plt.ylabel('Выборочные квантили')
+
+plt.tight_layout()
+plt.show()
--- normal.py
+++ normal.py
@@ -1,36 +1,69 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 mu = 12
 sigma = 2.7
 sample_size = 50
 
 # Генерация выборки
-sample = np.random.normal(mu, sigma, sample_size)
-
-# Вывод выборки
-print(sample)
+sample = np.random.normal(loc=mu, scale=sigma, size=sample_size)
 
 # Описательная статистика
-print("Описательная статистика:")
-print("Среднее:", np.mean(sample))
-print("Стандартная ошибка:", np.std(sample) / np.sqrt(len(sample)))
-print("Медиана:", np.median(sample))
-print("Мода:", stats.mode(sample))
-print("Стандартное отклонение:", np.std(sample))
-print("Дисперсия выборки:", np.var(sample))
-print("Эксцесс:", stats.kurtosis(sample))
-print("Асимметричность:", stats.skew(sample))
-print("Интервал:", )
-print("Минимум:", np.amin(sample))
-print("Максимум:", np.amax(sample))
-print("Сумма:", np.sum(sample))
-print("Счет:", len(sample))
-print("Уровень надежности:", )
+print('Описательная статистика:')
+print('Среднее:', np.mean(sample))
+print('Стандартная ошибка:', np.std(sample) / np.sqrt(len(sample)))
+print('Медиана:', np.median(sample))
+print('Мода:', stats.mode(sample))
+print('Стандартное отклонение:', np.std(sample))
+print('Дисперсия выборки:', np.var(sample))
+print('Эксцесс:', stats.kurtosis(sample))
+print('Асимметричность:', stats.skew(sample))
+print('Доверительный интервал для математического ожидания (95,0%):', stats.t.interval(confidence=0.95, df=len(
+    sample)-1, loc=np.mean(sample), scale=stats.sem(sample)))
+print('Минимум:', np.amin(sample))
+print('Максимум:', np.amax(sample))
+print('Сумма:', np.sum(sample))
+print('Счет:', len(sample))
+print('Доверительный интервал для дисперсии (95,0%):',  stats.t.interval(confidence=0.95, df=len(sample)-1,
+      loc=np.var(sample), scale=2*np.var(sample)/len(sample)))
+
+plt.figure(figsize=(14, 10))
 
 # Гистограмма
+plt.subplot(2, 3, 1)
 sns.histplot(sample, kde=True)
 plt.title('Гистограмма')
+plt.xlabel('Значение')
+plt.ylabel('Частота')
+
+# Полигон
+plt.subplot(2, 3, 2)
+sns.kdeplot(sample, cumulative=False)
+plt.title('Полигон')
+plt.xlabel('Значение')
+plt.ylabel('Плотность')
+
+# Кумулянта
+plt.subplot(2, 3, 3)
+sns.kdeplot(sample, cumulative=True)
+plt.title('Кумулянта')
+plt.xlabel('Значение')
+plt.ylabel('Вероятность')
+
+# Ящик с усами
+plt.subplot(2, 3, 4)
+sns.boxplot(sample)
+plt.title('Ящик с усами')
+plt.xlabel('Значение')
+
+# Q-Q Plot
+plt.subplot(2, 3, 5)
+stats.probplot(sample, plot=plt)
+plt.title('Q-Q Plot')
+plt.xlabel('Теоретические квантили')
+plt.ylabel('Выборочные квантили')
+
+plt.tight_layout()
 plt.show()
